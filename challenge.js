c2f03dd756bd8892575c2343ab1a4be105be003a
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-21 17:59:41 -0400

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,6 +1,7 @@
 function right(){
   var h1 = document.getElementById("h1");
   h1.innerText = "Correct! Click me to go to the next challenge you want to try.";
+  h1.style.color = "green";
   h1.addEventListener("click", function(){
     h1.style.display = "none";
     var nextChallenge = prompt("Enter the next challenge you want to try > ");
@@ -16,6 +17,7 @@ function right(){
 function wrong(){
   var h1 = document.getElementById("h1");
   h1.innerText = "Wrong! Click me to undisplay this. ";
+  h1.style.color = "red";
   h1.addEventListener("click", function(){
     h1.innerText = "";
   })
