4ee72c2a141b78755ee1e1e3201fe363be179420
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-21 15:21:22 -0400

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -11,7 +11,7 @@ function right(){
 function wrong(){
   var h1 = document.getElementById("h1");
   h1.innerText = "Wrong! Click me to undisplay this.";
-  h1.addEventListenr("click", function(){
+  h1.addEventListener("click", function(){
     h1.style.display = "none";
-  });
+  })
 }
