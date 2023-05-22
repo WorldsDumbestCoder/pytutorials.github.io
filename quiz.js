bcfd04587de651b62ae7ea1ca14b3c5c5037b04e
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-22 17:38:05 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -4,6 +4,15 @@ function increase_correct(){
 }
 function check_answer(){
     let ans = document.querySelector("input[name=check]:checked");
+    if(ans.value == ""){
+       try{
+          throw "Enter a value!";
+       }catch(err){
+         var h1 = document.createElement("h1");
+         h1.innerText = err;
+         document.body.appendChild(h1);
+       }
+    }
     if (ans.value != 4) {
       var h1 = document.createElement("h1");
       h1.innerText = "Try Again!";
