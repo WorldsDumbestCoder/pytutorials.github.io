7aa44f662fb2fa69510ee193fadd64f572d2960d
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-22 19:42:47 -0400

    Update search.js

--- search.js
+++ search.js
@@ -1,18 +1,10 @@
-function validate(){
-  var s = document.getElementById("search").value;
-  try{
-    if(s == ""){
-      throw "Enter a page!";
-    }else{
-      search();
-    }
-  }catch(e){
-      var h1 = document.createElement("h1");
-      h1.innerText = e;
-      document.body.appendChild(h1);
-  }
-}
 function search(){
   var s = document.getElementById("s").value;
-  window.location.assign("https://pytutorials.github.io/" + s);
+  if(s != ""){
+    window.location.assign("https://pytutorials.github.io/" + s);
+  }else{
+    var h1 = document.createElement("h1");
+    h1.innerText = "Enter a page!";
+    document.body.appendChild(h1);
+  }
 }
