9ec7a22f89745b0b93a0175dd3637c7a6d970253
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-22 12:56:51 -0400

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,12 @@
+function check_answer(){
+  var ans = document.getElemenetById("answer").value;
+  if(ans.value != 6){
+    var h1 = document.createElement("h1");
+    h1.innerText = "Try Again!";
+    document.body.appendChild(h1);
+  }else{
+    var h1 = document.createElement("h1");
+    h1.innerText = "Correct!";
+    document.body.appendChild(h1);
+  }  
+}
