c079e5d14c981792a25051dc641c286010a12631
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-22 14:44:49 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,14 +1,13 @@
 function check_answer(){
-  var ans = document.getElementById("answer");
-  if(ans.value != 4){
+  let ans = document.querySelector("input[name=check]:checked");
+  if (ans.value != 4) {
     var h1 = document.createElement("h1");
     h1.innerText = "Try Again!";
     document.body.appendChild(h1);
   }
-  if(ans.value == 4){
+  if (ans.value == 4) {
     var h1 = document.createElement("h1");
     h1.innerText = "Correct!";
     document.body.appendChild(h1);
-  }  
+  }
 }
-
