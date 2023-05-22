70d51f8bb0450c9a5d991ddf7559dfd22c0a8e1d
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-22 13:27:16 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,11 +1,11 @@
 function check_answer(){
   var ans = document.getElementById("answer");
-  if(ans.value != 6){
+  if(ans.value != 4){
     var h1 = document.createElement("h1");
     h1.innerText = "Try Again!";
     document.body.appendChild(h1);
   }
-  if(ans.value == 6){
+  if(ans.value == 4){
     var h1 = document.createElement("h1");
     h1.innerText = "Correct!";
     document.body.appendChild(h1);
