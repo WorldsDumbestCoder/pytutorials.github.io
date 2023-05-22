12ef0c1e50c160429c8eeef1767e3fd1d35d302a
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-22 17:33:49 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -3,24 +3,17 @@ function increase_correct(){
   return answers_correct+1;
 }
 function check_answer(){
-  try{
     let ans = document.querySelector("input[name=check]:checked");
     if (ans.value != 4) {
       var h1 = document.createElement("h1");
       h1.innerText = "Try Again!";
       h1.id = "header";
       document.body.appendChild(h1);
-    }catch(err){
-        document.write("An unexpected error happened while reciving your answers. " + err);
-  }
-  if (ans.value == 4) {
-    try{
+    }
+    if (ans.value == 4) {
       var h1 = document.createElement("h1");
        h1.innerText = "Correct!";
        h1.id = "header";
        document.body.appendChild(h1);
-    }catch(err){
-       document.write("An unexpected error happend while reciving your answers. " + err)
     }
-  }
 }
