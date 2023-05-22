e9aae626f43f47379b692b71d6e51e500da72505
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-22 19:21:44 -0400

    Update search.js

--- search.js
+++ search.js
@@ -5,6 +5,9 @@ function search(){
       throw "Enter a tutorial!";
     }else{
       window.location.assign("tutorials/" + s);
-    }
+    }catch(e){
+      var h1 = document.createElement("h1");
+      h1.innerText = e;
+      document.body.appendChild(e);
   }
 }
