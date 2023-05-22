ada999b71dde54f5e3d8efadf14a8626cb114964
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-22 13:22:06 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 function check_answer(){
-  var ans = document.getElemenetById("answer");
+  var ans = document.getElementById("answer");
   if(ans.value != 6){
     var h1 = document.createElement("h1");
     h1.innerText = "Try Again!";
