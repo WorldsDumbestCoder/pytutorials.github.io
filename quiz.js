69508954487ec32f4d38269436a88ffa7e58eda0
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-23 18:32:11 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -34,5 +34,7 @@ function check_answer(){
       h1.innerText = "Correct!";
       document.body.appendChild(h1);
       answers_correct = answers_correct+1;
+      var p = document.createElement("p");
+      p.innerText = "Answers Correct/Wrong: " + answers_wrong + "/ Answers Correct: " + answers_correct;
    }
 }
