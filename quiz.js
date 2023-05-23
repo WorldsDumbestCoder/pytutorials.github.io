3b071012a431d6e6f65c580de110b65c63a7b316
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-23 19:15:39 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -40,5 +40,18 @@ function check_answer(){
       h1.innerText = "Correct!";
       document.body.appendChild(h1);
       answers_correct = answers_correct+1;
+      var p = document.createElement("p");
+      p.innerText = "Answers Correct/Wrong: " + answers_wrong + " /Correct: " + answers_correct;
+      document.body.appendChild(p);
+      answers_wrong = answers_wrong+1;
+      var calc = answers_correct/4;
+      var calcTimesHund = calc*100;
+      if(calcTimesHund < 50){
+         alert("You failed this quiz!");
+         document.write("you got " + calcTimesHund + "%");
+      }else{
+         alert("Good Job! YOU PASSED THIS QUIZ!");
+         document.write("you got " + calcTimesHund + "%");
+      }
    }
 }
