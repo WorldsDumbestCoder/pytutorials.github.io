49d19e2778f8980b002bae894133ac118934818e
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-23 18:06:37 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -21,11 +21,15 @@ function checkbox_checked(){
 function check_answer(){
    let ans = document.querySelector('input[name=check]:checked');
    if (ans.value != 4) {
+      var h1 = document.createElement("h1");
       h1.innerText = "Try Again!";
+      document.body.appendChild(h1);
       answers_wrong = answers_wrong+1;
    }
    if (ans.value == 4) {
+      var h1 = document.createElement("h1")
       h1.innerText = "Correct!";
+      document.body.appendChild(h1);
       answers_correct = answers_correct+1;
    }
 }
