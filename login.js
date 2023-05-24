3392354021bf8e9bcb41098d7ac06288d681b6a8
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-23 21:17:13 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,9 @@
 if(!localStorage.getItem("username")){
  document.write("<a href='javascript:signup()'>Create a username for free</a>");
+ window.location.assign("index.html");
 }else{
   document.write("<a href='javascript:removeUsername();'>Logout</a>");
+  alert("Welcome Back " + localStorage.getItem("username"));
 }
 function removeUsername(){
   localStorage.removeItem("username");
@@ -9,4 +11,5 @@ function removeUsername(){
 function signup(){
   var username = prompt("Create a username > ");
   localStorage.setItem("username", username);
+  window.location.assign("index.html");
 }
