fda73428992061e741e597a57770aa2bcc3df6ec
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-23 21:10:48 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,4 +1,5 @@
 if(!localStorage.getItem("username")){
   var username = prompt("Create a username > ");
   localStorage.setItem("username", username);
-}
+}else{
+  document.write("<a href='javascript:localStorage.removeItem('username');'>Logout</a>");
