f9229f0b499c949ec16355b0481d1ccdad7dc7fe
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-23 21:20:46 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,6 +1,5 @@
 if(!localStorage.getItem("username")){
  document.write("<a href='javascript:signup()'>Create a username for free</a>");
- window.location.assign("index.html");
 }else{
   document.write("<a href='javascript:removeUsername();'>Logout</a>");
   alert("Welcome Back " + localStorage.getItem("username"));
