9c4b42fa8a8968836f7a2809cd3bc1f5b1b18acd
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-05-27 18:02:47 -0400

    Create tutorials.js

---
+++ tutorials.js
@@ -0,0 +1,8 @@
+var code=['<a href="tutorial6">Tutorial 6</a> <a href="tutorial7">Tutorial 7</a>  <a href="tutorial8">Tutorial 8</a>  <a href="tutorial9">Tutorial 9</a> <a href="tutorial10">Tutorial 10</a>']
+    if(localStorage.getItem("username")){
+      document.write(code);
+    }else{
+      var p = document.createElement("p");
+      p.innerText = "Signup to unlock more tutorials <a href='jacascript:signup()'>Signup</a>";
+      document.body.appendChild(p);
+    }
