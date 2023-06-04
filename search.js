3e7b8ae59d7dc1015c2e3ae7dc1a90254e19b8cb
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-04 16:47:33 -0400

    Update search.js

--- search.js
+++ search.js
@@ -4,5 +4,6 @@ function search(){
      window.location.assign("https://pytutorials.github.io/" + searchValue);
   }else{
     document.write("<h1>Enter a page!</h1> <a href='index'>Go Home</a>");
+    document.write("<a href='c'>c</a>");
   }
 }
