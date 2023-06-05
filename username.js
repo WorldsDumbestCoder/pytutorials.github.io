7e316c4b81409f5788d5dc6a7e3923f93cf84673
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-04 21:34:04 -0400

    Update username.js

--- username.js
+++ username.js
@@ -1,5 +1,13 @@
+if(!localStorage.getItem("username")){
+  alert("Redirecting you to the signup page");
+  window.location.assign("signup");
+}
 function change_username(){
   var username = prompt("Enter your new username > ");
   localStorage.removeItem("username");
   localStorage.setItem("username", username);
+}
+function remove_username(){
+  localStorage.removeItem("username");
+  alert("You have been signed out! Refresh the page to see the changes");
 }
