993b5688859d467be811f0cf5591cd6ee3e4964d
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-04 21:59:26 -0400

    Update username.js

--- username.js
+++ username.js
@@ -13,14 +13,9 @@ function remove_username(){
 }
 function home_settings(){
   var form = document.getElementById("form").value;
-  if(form.value == 1){
-     var popup_username = true;
-     localStorage.setItem("popup_username", popup_username);
-     alert(popup_message);
-  }
   if(form.value == 2){
-    var popup_username = false;
-    localStorage.setItem("popup_username", popup_username);
-    alert(popup_message);
+     var popup_username = false;
+     localStorage.setItem("popup_username=false", popup_username);
+     alert(popup_message);
   }
 }
