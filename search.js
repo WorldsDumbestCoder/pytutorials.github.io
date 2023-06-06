ee33a4a6c94ecc61f8a1a18a1038c94f51df5d31
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-05 20:27:41 -0400

    Update search.js

--- search.js
+++ search.js
@@ -4,7 +4,7 @@ function search(){
      window.location.assign("https://pytutorials.github.io/" + searchValue);
   }else{
     document.write("<h1 style='font-family:cursive;'>Enter a page!</h1><br/>");
-    document.write("<a href='index'>Go Home</a>");
+    document.write("<a href='index'>Go Home</a><br/>");
     document.write("<em>Thanks for using PyTutorials!</em>");
   }
 }
