5dbdd373377b32be9b7f5eeeda7880ecba082727
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-13 13:47:40 -0400

    Update username.js

--- username.js
+++ username.js
@@ -2,17 +2,14 @@ if(!localStorage.getItem("username")){
   alert("Redirecting you to the signup page");
   window.location.assign("signup");
 }
-function undisplay(){
-  docucument.getElementById("p").style.display = "none";
-}
 function change_username(){
   var username = prompt("Enter your new username > ");
   localStorage.removeItem("username");
   localStorage.setItem("username", username);
   var p = document.createElement("p"):
   p.innerText = "Your username has been saved. Click me to see undisplay this.";
-  p.onclick = undisplay();
-  p.id = "p";
+  p.style.color = "green";
+  p.style.align = "center";
   document.body.appendChild(p);
 }
 function remove_username(){
@@ -26,37 +23,40 @@ function dont_username_popup(){
     p.innerText = "Changes Saved! Click me to undisplay this.";
     p.style.align = "center";
     p.style.color = "green";
-    p.id = "p";
-    p.onclick = undisplay();
     document.body.appendChild(p);
   }else{
     var p = document.createElement("p");
-    p.innerText = "This is already set! Click me to undisplay this.";
+    p.innerText = "This is already set!";
     p.style.align = "center";
-    p.id = "p";
-    p.onclick = p.style.display = "none";
     p.style.color = "red";
-    p.onclick = undisplay();
+    p.id = "p";
+    var button = document.createElement("button");
+    button.innerText = "Click me to undisplay the paragraph";
+    button.onclick = document.getElementById("p").style.display = "none";
+    document.body.appendChild(button);
     document.body.appendChild(p);
+    
  }
 }
 function popup_username(){
   if(localStorage.getItem("popup_username=false")){
     localStorage.removeItem("popup_username=false");
     var p = document.createElement("p");
-    p.innerText = "Changes Saved! Click me to undisplay this.";
+    p.innerText = "Changes Saved!";
     p.style.align = "center";
     p.style.color = "green";
-    p.onclick = undisplay();
     p.id = "p";
     document.body.appendChild(p);
+    var button = document.createElement("button");
+    button.innerText = "Click me to display the paragraph";
+    button.onclick = 
+    document.body.appendChild(button);
   }else{
     var p = document.createElement("p");
     p.innerText = "This is already set! Click me to undisplay this";
     p.style.align = "center";
     p.style.color = "red";
     p.id = "p";
-    p.onclick = undisplay();
     document.body.appendChild(p);
   }
 }
