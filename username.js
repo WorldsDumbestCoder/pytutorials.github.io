0ad201fdac6e18584e9a13615ab15fecd63da369
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-18 16:41:18 -0400

    Update username.js

--- username.js
+++ username.js
@@ -1,28 +1,22 @@
 function show_user() {
-	let username = localStorage.getItem("username");
-  alert(username);
+	alert(localStorage.getItem("username");
 }
-
 function add_user() {
 	let username = prompt("Enter your new username > ");
-  localStorage.removeItem("username");
-  localStorage.setItem("username", username);
-  
-  let p = document.getElementById("username");
-  let message = `username ${username} has been saved`;
-  
-  p.innerText = message;
-  p.style.color = "green";
-  p.style.textAlign = "center";
+	localStorage.removeItem("username");
+	localStorage.setItem("username", username);
+	let p = document.getElementById("username");
+	let message = `username ${username} has been saved`;
+	p.innerText = message;
+	p.style.color = "green";
+	p.style.textAlign = "center";
   
 }
 
 function remove_user(){
 	localStorage.removeItem("username");
-  alert("You have been signed out! Refresh the page to see the changes");
-  
-  let p = document.getElementById("username");
-  let message = '&nbsp;';
-  
-  p.innerHTML = message;
+	alert("You have been signed out! Refresh the page to see the changes");
+	let p = document.getElementById("username");
+	let message = '&nbsp;';
+	p.innerHTML = message;
 }
