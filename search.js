2cad592c9d299b9ba9a323559a2d33b38083b3e8
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-18 21:15:55 -0400

    Update search.js

--- search.js
+++ search.js
@@ -5,9 +5,14 @@ function search(){
   }else{
     var h1 = document.createElement("h1");
     h1.innerText = "Enter a page!";
+    h1.style.position = "absolite";
+    h1.style.bottom = "100%";
     var br = document.createElement("br");
     var em = document.createElement("em");
     em.innerText = "Thanks for using PyTutorials!";
+    em.style.position = "absolute";
+    em.style.top = "100%":
+    em.style.right = "50%";
     document.body.appendChild(h1);
     document.body.appendChild(br);
     document.body.appendChild(em);
