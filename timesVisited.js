eeb306e8fdc82368e9a9d62ac25db44df7d5e3e8
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-18 20:15:10 -0400

    Update timesVisited.js

--- timesVisited.js
+++ timesVisited.js
@@ -1,7 +1,7 @@
 let timesVisited = 1;
 if(!localStorage.getItem("timesvisited")){
   localStorage.setItem("timesvisited", timesVisited);
-  alert("You visited " + localStorage.getItem("timesvisited" + " time(s)");
+  alert("You visited " + localStorage.getItem("timesvisited")+ " time(s)");
 }else{
  timesVisited+=1;
  alert("You visited this page " + localStorage.getItem("timesvisited") + " times");
