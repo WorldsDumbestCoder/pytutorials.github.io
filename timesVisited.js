11b346a7d7e602b56870139051831a33393fa144
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-18 20:33:37 -0400

    Update timesVisited.js

--- timesVisited.js
+++ timesVisited.js
@@ -3,7 +3,7 @@ if(!localStorage.getItem("timesvisited")){
   localStorage.setItem("timesvisited", timesVisited);
   alert("You visited " + localStorage.getItem("timesvisited")+ " time(s)");
 }else{
- timesVisited = parseInt(localStorage.getItem("timesvisited"))+1;
- localStorage.setItem("timesvisited", timesVisited);
- alert("You visited this page " + localStorage.getItem("timesvisited") + " times");
+ timesVisited = parseInt(localStorage.getItem("timesvisited"))+1;//Change timesVisited into an intenger than increase the amount of times visited.
+ localStorage.setItem("timesvisited", timesVisited);//set The times visited
+ alert("You visited this page " + localStorage.getItem("timesvisited") + " times");//output the times visited.
 }
