71c426be6279e468723660a48670bf1aaa83d5d7
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-24 13:11:04 -0400

    Update search.js

--- search.js
+++ search.js
@@ -3,10 +3,6 @@ function search(){
   if(searchValue != ""){
     window.location.assign(searchValue);
   }else{
-    var h1 = document.createElement("h1");
-    h1.innerText = "Search for a page";
-    h1.style.position = "absolute";
-    h1.style.bottom = "100%";
-    document.body.appendChild(h1);
+    alert("Fill out the form properly!");
   }
 }
