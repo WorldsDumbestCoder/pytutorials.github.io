62f68a15eddb66b59180e23ae175315d0087a28a
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-24 14:07:03 -0400

    Update username.js

--- username.js
+++ username.js
@@ -1,24 +1,19 @@
-function show_user() {
-	let p = document.getElementById("username");
-	let username = localStorage.getItem("username");
-	p.innerText = `Your username is ${username}`;
-	p.style.textAlign = "center";
-	p.style.color = "lightblue";
+function show_user(){
+	var username = localStorage.getItem("username");
+	var user = document.getElementById("username");
+	user.value = "Your username is " + user;
+	user.style.color = "green";
 }
-function add_user() {
-	let username = prompt("Enter your new username > ");
-	localStorage.removeItem("username");
-	localStorage.setItem("username", username);
-	let p = document.getElementById("username");
-	let message = `username ${username} has been saved`;
-	p.innerText = message;
-	p.style.color = "green";
-	p.style.textAlign = "center";
+function add_user(){
+	var user = prompt("Create your username: ");
+	localStorage.setItem("username", user);
+	var username = document.getElementById("username");
+	username.value = "Your username is " + user;
+	username.style.color = "lightblue";
 }
 function remove_user(){
+	var username = document.getElementById("username");
+	username.value = "Your username " + localStorage.getItem("username") + " has been removed";
+	username.style.color = "red";
 	localStorage.removeItem("username");
-	alert("You have been signed out! Refresh the page to see the changes");
-	let p = document.getElementById("username");
-	let message = '&nbsp;';
-	p.innerHTML = message;
 }
