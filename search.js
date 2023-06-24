4447aa3105b60d1e43cc37c5e37e3ebcb9d75d20
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-24 13:53:22 -0400

    Update search.js

--- search.js
+++ search.js
@@ -2,6 +2,10 @@ function search(){
   var searchValue = document.getElementById("search").value;
   if(searchValue != ""){
     window.location.assign(searchValue);
+    if(window.undefined){
+     document.write("<h1>Search Value Is Not A Page!</h1><br/>");
+     document.write("<a href='javascript:window.location.reload();'>Reload the page</a>");
+    }
   }else{
     alert("Fill out the form properly!");
   }
