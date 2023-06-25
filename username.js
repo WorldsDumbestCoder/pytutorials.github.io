e7d99e035da7d4a1a54029b8415c797635f270ba
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-25 13:10:16 -0400

    Update username.js

--- username.js
+++ username.js
@@ -45,12 +45,14 @@ function remove_user(){
 		var username = document.createElement("p");
 		p.innerText = "Your username has been removed. Reload the page to see the changes.";
 		p.style.textAlign = "center";
+		p.style.color = "green";
 		document.body.appendChild(p);
 		localStorage.removeItem("username");
 	}else{
 		var p = document.createElement("p");
 		var a = document.createElement("a");
 		p.innerText = "Username not found";
+		p.style.color = "red";
 		p.style.textAlign = "center";
 		a.innerText = "Create a username";
 		a.href = "javascript:add_user()";
