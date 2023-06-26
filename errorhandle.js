a2d61833c0e8c378e3184ace2fb9475970ed3448
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-26 11:04:51 -0400

    Update errorhandle.js

--- errorhandle.js
+++ errorhandle.js
@@ -1,5 +1,7 @@
 if(window.undefined){
-  window.location.assign("404");
+  document.write("<h1>404 - Page Not Found</h1><br/>");
+  document.write("<p>The page you were looking for does not exist</p><br/>");
+  document.write("<a href='index.html'>Go Home</a><br/>");
 }
 if(window.onerror){
   document.write("<h1 style='color:white;'>There was an unexpected error with the website.</h1>");
