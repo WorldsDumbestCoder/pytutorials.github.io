3f3e51374666d555059ea0f5f4131093d4e94336
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-30 16:53:17 -0400

    Update errorhandle.js

--- errorhandle.js
+++ errorhandle.js
@@ -2,7 +2,10 @@ if(window.undefined){
   const xhttp  = new XMLHttpRequest();
   xhttp.onload = function(){
     var div = document.createElement("div");
-    div.innerHTML = this.responseText;
+    div.innerHTML =  this.responseText;
+    var note = document.createElement("p");
+    note.innerText = "Powered by JavaScript";
+    document.body.appendChild(note);
   }
   xhttp.open("GET", "404.html");
   xhttp.send();
